40ac07f5e78d95a83d58a267e15087485622ef1b
Cweili <3weili@gmail.com>
2019-07-22 17:28:39 +0800

    fix: parse integer

--- cli.js
+++ cli.js
@@ -2,16 +2,18 @@
 const cmd = require('commander');
 const pkg = require('./package');
 
+const toInt = str => parseInt(str, 10);
+
 cmd
   .version(pkg.version)
   .usage('[options]')
   .option('-d, --db-host <hostname>', 'database host', '127.0.0.1')
-  .option('-P, --db-port <port>', 'database port', parseInt, 80)
+  .option('-P, --db-port <port>', 'database port', toInt, 80)
   .option('-u, --db-user <username>', 'database user')
   .option('-p, --db-password <password>', 'database password')
   .option('-n, --db-name <database>', 'database name', 'taotie')
-  .option('-H, --http <port>', 'HTTP server port', parseInt, 80)
-  .option('-U, --udp <port>', 'UDP log receiver port', parseInt, 514)
+  .option('-H, --http <port>', 'HTTP server port', toInt, 80)
+  .option('-U, --udp <port>', 'UDP log receiver port', toInt, 514)
   .option('-i --interval <secode>', 'save logs interval')
   .option('-D, --dev', 'development mode')
   .parse(process.argv);
